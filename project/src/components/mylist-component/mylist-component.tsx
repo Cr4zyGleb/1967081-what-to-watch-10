175ff78932748aed8d4acd40e0aa1371df62d471
Cr4zyGleb <Cr4zyGleb@gmail.com>
2022-08-14 13:45:58 +0300

    7.12. Добро пожаловать, или посторонним вход воспрещён (часть 2) v3

--- project/src/components/mylist-component/mylist-component.tsx
+++ project/src/components/mylist-component/mylist-component.tsx
@@ -4,12 +4,12 @@ import { AppRoute } from '../../const';
 function MyListComponent(): JSX.Element {
 
   return (
-    <button className="btn btn--list film-card__button" type="button">
+    <button style = {{color : 'inherit', textDecoration : 'none' }} className="btn btn--list film-card__button" type="button">
       <svg viewBox="0 0 19 20" width="19" height="20">
         <use xlinkHref="#add"></use>
       </svg>
       <Link to={AppRoute.MyList}>
-        <span className = "my-list">My list</span>
+        <span >My list</span>
       </Link>
       <span className="film-card__count">9</span>
     </button>
